da0c0ff0340675865e4b5c7c83e104b6f74bbea9
AM47-hub <andrewmawer74@gmail.com>
2026-04-29 16:18:05 +1000

    Update app.py

--- app.py
+++ app.py
@@ -174,7 +174,7 @@ def process():
                                 if days_ahead == 0: days_ahead = 7
                                 view_date = anchor_dt + timedelta(days=days_ahead)
                                 if pref == 'next' and anchor_dt.weekday() < target_weekday:
-                                view_date += timedelta(days=7)
+                                    view_date += timedelta(days=7)
 
                     if "1Found" in source: 
                         # Append a new object for PAST match found
