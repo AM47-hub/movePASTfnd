23328732b8b3c6c81e74b06e31ad72a6b6cd35dd
AM47-hub <andrewmawer74@gmail.com>
2026-05-04 14:04:43 +1000

    Update app.py

--- app.py
+++ app.py
@@ -12,6 +12,8 @@ REPAIRS = {
     'five': '5', 'six': '6',
     'seven': '7', 'eight': '8', 'ate': '8',
     'nine': '9', 'zero': '0', 'none':'0', 'nill':'0',
+    'eleven': '11', 'twelve': '12', 'thirteen': '13', 'fourteen': '14',
+    'fifteen': '15', 'sixteen': '16', 'seventeen': '17', 'eighteen': '18', 'nineteen': '19',
     'twenty': '20', 'thirty': '30', 'fourty':'40', 'fifty':'50',
     'dash': '-', '—': '-'
 }
